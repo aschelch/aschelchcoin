552e400a710e9ff9a0bfae730acc9d95f3147a59
Aurélien Schelcher <aurelien.schelcher@gmail.com>
2021-10-03 20:48:27 +0200

    feat(coin): first commit

---
+++ contracts/Token.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+
+    constructor () ERC20("AschelchCoin", "ASC") {
+        _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
+    }
+}
